051a8ec30ebfe44f3416a4f68b7daee2022a0b54
Vishal Pal <vishalpal1215@gmail.com>
2023-03-28 01:54:37 +0530

    Somnath Temple Js update

--- somnath/somnath.js
+++ somnath/somnath.js
@@ -14,9 +14,11 @@ console.log(artDiv);
 
 history.addEventListener("click", () => {
   console.log("History Button clicked");
+
   history.style.borderColor = "rgb(120, 110, 199)";
   temple.style.borderColor = "rgb(32, 12, 114)";
   art.style.borderColor = "rgb(32, 12, 114))";
+  
   hisDiv.style.display = "block";
   temDiv.style.display = "none";
   artDiv.style.display = "none";
@@ -24,20 +26,26 @@ history.addEventListener("click", () => {
 
 temple.addEventListener("click", () => {
   console.log("Visions Button clicked");
+
   history.style.borderColor = "rgb(32, 12, 114)";
   temple.style.borderColor = "rgb(120, 110, 199)";
   art.style.borderColor = "rgb(32, 12, 114)";
+
   temDiv.style.display = "block";
   hisDiv.style.display = "none";
   artDiv.style.display = "none";
+
 });
 
 art.addEventListener("click", () => {
   console.log("goals Button clicked");
+
   history.style.borderColor = "rgb(32, 12, 114)";
   temple.style.borderColor = "rgb(32, 12, 114)";
   art.style.borderColor = "rgb(120, 110, 199)";
+
   hisDiv.style.display = "none";
   temDiv.style.display = "none";
   artDiv.style.display = "block";
+
 });
